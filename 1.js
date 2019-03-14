07de8940b3515e048304df21bd3b545f6e1f1376
AronThomasMiller <aronthomasmiller@gmail.com>
2019-03-14 23:26:04 +0200

    js

---
+++ 1.js
@@ -0,0 +1,127 @@
+//ПЕРШЕ ЗАВДАННЯ
+
+/* var chooseBigger = (a,b) =>{
+if (a>b) {
+    return a;
+    }
+else return b;
+}
+console.log(chooseBigger(1,2)); */
+
+
+//ДРУГЕ ЗАВДАННЯ 
+
+
+/* var arr = [1, 3, 6, 5, 11, 4, 2, 16, 21, 20];
+
+var findMax = (array) => {
+    var max = array[0];
+    array.forEach(element => {
+        if (max <= element) {
+            max = element;
+        }
+    });
+    return max;
+}
+var findMin = (array) => {
+    var min = array[0];
+    array.forEach(element => {
+        if (min >= element) {
+            min = element;
+        }
+    });
+    return min;
+}
+console.log(findMax(arr));
+console.log(findMin(arr)); */
+
+//ТРЕТЄ ЗАВДАННЯ 
+
+/* var array = [1, 3, 6, 5, 11, 4, 2, 16, 21, 20];
+var filterArray = (array,minVal,maxVal) =>{
+    var arr1 = [];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (element < maxVal && element > minVal) {
+            arr1.push(element);
+        }
+    }
+    return arr1;
+}
+console.log(filterArray(array,1,21)); */
+
+//ЧЕТВЕРТЕ ЗАВДАННЯ
+
+/* function wordsCounter(str) { 
+    return str.split(" ").length;
+}
+console.log(wordsCounter("Hello, from Automaton team")); */
+  
+//П'ЯТЕ ЗАВДАННЯ
+/* var calculator = {
+    add: function(x, y) {
+        return x + y;
+    },
+    subtraction: function (x, y) {
+        return x - y;
+    },
+    multiply: function(x, y) {
+        return x * y;
+    },
+    divide: function (x, y) {
+        return x / y;
+    }
+};
+console.log(calculator.add(4,10));
+console.log(calculator.subtraction(4,10));
+console.log(calculator.multiply(4,10));
+console.log(calculator.divide(4,10));
+console.log("x+x*5-y/2 = " + calculator.subtraction(calculator.add(4,calculator.multiply(4,5)),calculator.divide(10,2))); */
+
+//ШОСТЕ ЗАВДАННЯ
+/* 
+var str = "Implement function that takes two parameters. First one is string, second one is Array of words. function should return Array of objects with 2 properties. First is Word, second is amount of times this word can be found in text. Be careful since word:Anna and anna is the same. you can use method .toLowerCase to make words similar.";
+var array = ["is" , "function"];
+function taskSix (str , array) {
+    var arr = [];
+    var counter = 0;
+        array.forEach(element => {
+            var position = 0;
+            while (true) {
+                var foundPos = str.indexOf(element.toLowerCase(), position);
+                if (foundPos == -1) break;
+                position = foundPos + 1; 
+                counter ++;
+            }
+        var object = {Word: element, Number: counter};
+        arr.push(object);
+        counter = 0;
+        });
+return arr;
+}
+var result = taskSix(str , array);
+result.forEach(element => {
+    console.log(element.Word  + " " + element.Number);
+}); */
+
+//СЬОМЕ ЗАВДАННЯ
+
+/* function isOdd (number) {
+    if (number % 2 !== 0)
+    return true;
+    else return false;
+}
+function taskSeven(array) {
+    var arr = [];
+    array.forEach(element => {
+       var result = isOdd(element);
+       var object = {Digit: element , Function: result};
+       arr.push(object);
+    });
+    return arr;
+}
+var array = [1 , 2 ,3 ,4 ,5];
+var mass = taskSeven(array);
+mass.forEach(element => {
+    console.log(element.Digit + " " + element.Function)
+}); */
